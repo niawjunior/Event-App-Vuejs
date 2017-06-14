f6b46214c4fc2b21c0e1e0f1ef37a841d11c79ab
niawkung <niawkung2@gmail.com>
2017-06-14 18:40:05 +0700

    crerate function signup

--- src/components/Signin.vue
+++ src/components/Signin.vue
@@ -1,43 +1,42 @@
 <template>
-<div class="col-md-12" >
- <div class="from-inline">
-       <h3>Sign In</h3>
-       <div class="form-group">
-        <input type="text" placeholder="email" class="form-control" v-model="email"/>
-       </div>
-       <div class="form-group">
-        <input type="password" placeholder="password" class="form-control" v-model="password">
+<div class="col-md-12">
+  <div class="from-inline">
+    <h3>Sign In</h3>
+    <div class="form-group">
+      <input type="text" placeholder="email" class="form-control" v-model="email" />
+    </div>
+    <div class="form-group">
+      <input type="password" placeholder="password" class="form-control" v-model="password">
     </div>
     <button class="btn btn-primary btn-block " @click="signIn">Sign In</button>
+  </div>
+  <br>
+  <p>{{error.message}}</p>
 </div>
-   
-    
-        
-       <br>
-       <p>{{error.message}}</p>
-    </div>
 </template>
 
 <script>
-import { firebaseApp } from '../firebaseApp'
+import {
+  firebaseApp
+} from '../firebaseApp'
 
-    export default {
-        data() {
-            return {
-                email: '',
-                password: '',
-                error: {
-                    message: ''
-                }
-            }
-        },
-        methods: {
-            signIn() {
-                firebaseApp.auth().signInWithEmailAndPassword(this.email, this.password)
-                .catch(error => {
-                    this.error = error
-                })
-            }
-        }
+export default {
+  data() {
+    return {
+      email: '',
+      password: '',
+      error: {
+        message: ''
+      }
     }
+  },
+  methods: {
+    signIn() {
+      firebaseApp.auth().signInWithEmailAndPassword(this.email, this.password)
+        .catch(error => {
+          this.error = error
+        })
+    }
+  }
+}
 </script>
---
+++ src/components/Signup.vue
@@ -0,0 +1,40 @@
+<template>
+<div class="col-md-12">
+  <div class="from-inline">
+    <h3>Sign Up</h3>
+    <div class="form-group">
+      <input type="text" placeholder="email" class="form-control" v-model="email" />
+    </div>
+    <div class="form-group">
+      <input type="password" placeholder="password" class="form-control" v-model="password">
+    </div>
+    <button class="btn btn-primary btn-block " @click="signUp">Sign Up</button>
+  </div>
+  <br>
+  <p>{{error.message}}</p>
+</div>
+</template>
+<script>
+    import { firebaseApp } from '../firebaseApp'
+
+    export default {
+        data() {
+            return {
+                email: '',
+                password: '',
+                error: {
+                    message: ''
+                }
+            }
+        },
+
+        methods: {
+            signUp() {
+                firebaseApp.auth().createUserWithEmailAndPassword(this.email, this.password)
+                .catch(error => {
+                    this.error = error
+                })
+            }
+        }
+    }
+</script>
