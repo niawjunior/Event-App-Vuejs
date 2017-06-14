4079788264d9696b4771e579dcd3b81d6ba25349
niawkung <niawkung2@gmail.com>
2017-06-14 16:27:18 +0700

    add firebase

---
+++ src/firebaseApp.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase'
+
+
+  const config = {
+    apiKey: "AIzaSyBUUCm8KSKfXcFfMl-XpLG-CxW3TZGv_KQ",
+    authDomain: "todolist-app-b6acc.firebaseapp.com",
+    databaseURL: "https://todolist-app-b6acc.firebaseio.com",
+    projectId: "todolist-app-b6acc",
+    storageBucket: "todolist-app-b6acc.appspot.com",
+    messagingSenderId: "507217730194"
+  };
