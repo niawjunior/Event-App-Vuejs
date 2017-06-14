5fe6884a40d3c4c3a40f9e326959ba53b129c747
niawkung <niawkung2@gmail.com>
2017-06-14 17:59:19 +0700

    dispatch data on firebase

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+import { mutations } from './mutations'
+import * as actions from './actions'
 Vue.use(Vuex)
 
 const state = {
@@ -8,5 +9,7 @@ const state = {
 }
 
 export default new Vuex.Store({
-    state
+    state,
+    mutations,
+    actions
 })
